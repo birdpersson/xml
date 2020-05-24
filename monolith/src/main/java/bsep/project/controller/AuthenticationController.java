632b80d4df16c8a8920b1bc3743063b4061f6244
birdpersson <aleksandarvujasin@gmail.com>
2020-05-24 22:12:44 +0200

    Inital project

---
+++ monolith/src/main/java/bsep/project/controller/AuthenticationController.java
@@ -0,0 +1,55 @@
+package bsep.project.controller;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import bsep.project.model.User;
+import bsep.project.model.UserTokenState;
+import bsep.project.security.TokenUtils;
+import bsep.project.security.auth.JwtAuthenticationRequest;
+
+@RestController
+@RequestMapping(value = "/auth", produces = MediaType.APPLICATION_JSON_VALUE)
+public class AuthenticationController {
+
+	@Autowired
+	private TokenUtils tokenUtils;
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+//	@Autowired
+//	private CustomUserDetailsService userDetailsService;
+
+	@PostMapping("/login")
+	public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtAuthenticationRequest authenticationRequest,
+			HttpServletResponse response) {
+
+		final Authentication authentication = authenticationManager
+				.authenticate(new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(),
+						authenticationRequest.getPassword()));
+
+		// Ubaci username + password u kontext
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+
+		// Kreiraj token
+		User user = (User) authentication.getPrincipal();
+		String jwt = tokenUtils.generateToken(user.getUsername());
+		int expiresIn = tokenUtils.getExpiredIn();
+
+		// Vrati token kao odgovor na uspesnu autentifikaciju
+		return ResponseEntity.ok(new UserTokenState(jwt, expiresIn));
+	}
+
+}
