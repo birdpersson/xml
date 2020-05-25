888c5e94f439cf8b29e39982f5ee91f5fa208ea4
mandy94 <majasavic199@gmail.com>
2020-05-25 19:13:01 +0200

    Divided monoliths

---
+++ monolith-xml/server/spring-security-example/src/main/java/rs/ac/uns/ftn/informatika/spring/security/service/impl/AdvertServiceImpl.java
@@ -0,0 +1,38 @@
+package rs.ac.uns.ftn.informatika.spring.security.service.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.stereotype.Service;
+
+import rs.ac.uns.ftn.informatika.spring.security.model.Advert;
+import rs.ac.uns.ftn.informatika.spring.security.repository.Advertrepository;
+import rs.ac.uns.ftn.informatika.spring.security.service.AdvertService;
+
+@Service
+public class AdvertServiceImpl implements AdvertService {
+	@Autowired
+	private Advertrepository repo;
+	
+
+	public List<Advert> findAll() throws AccessDeniedException {
+		List<Advert> result = repo.findAll();
+		return result;
+	}
+
+
+	@Override
+	public Optional<Advert> getAdById(Long id) {
+		
+		return repo.findById(id);
+	}
+
+
+	@Override
+	public void save(Advert ad) {
+		repo.save(ad);
+	}
+
+}
